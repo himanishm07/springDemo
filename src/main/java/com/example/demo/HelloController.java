78020b5c2169540560662c4d0d76e0eaf859c573
Himanish Munjal <himanism@amazon.com>
2021-01-26 22:37:08 +0530

    add 6

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot finally!!!!";
+        return "Greetings from Spring Boot finally what happened!!!!";
     }
 
 }
